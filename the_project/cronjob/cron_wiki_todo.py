a47c31fbd574768ba5c9234a387ab1b83438eaca
arkb2023 <ark.b2023@gmail.com>
2025-12-07 10:40:49 +0530

    Hardcoded cross-namespace service FQDN

--- the_project/cronjob/cron_wiki_todo.py
+++ the_project/cronjob/cron_wiki_todo.py
@@ -12,7 +12,10 @@ if "Location" in response.headers:
     wiki_url = response.headers["Location"]
     todo_text = f"Read {wiki_url}"
     print(f"todo text: {todo_text}")
-    TODO_API_URL = os.getenv("TODO_API_URL", "http://todo-backend-svc.project.svc.cluster.local:4567/todos/")
+    # fix: FQDN with project name wont work in multi branch deployment setup!
+    #TODO_API_URL = os.getenv("TODO_API_URL", "http://todo-backend-svc.project.svc.cluster.local:4567/todos/")
+    TODO_API_URL = os.getenv("TODO_API_URL", "http://todo-backend-svc:4567/todos/")
+    
     post_response = requests.post(TODO_API_URL, json={"text": todo_text})
     
     print(f"Created todo: Read {wiki_url}, Response status: {post_response.status_code}")
